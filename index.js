f24e35aa5921dceb8505e9970efca9ef01455b7a
Yeison <enmanuelquiterios@gmail.com>
2021-10-08 20:51:06 -0400

    commit tree 4eb1afb4ddad5906983549b693ae28f52fe7a8db

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Add your Circle class here
+
+
+class Circle {
+    constructor(num){
+        this.num = num;
+
+    }
+
+    get radius(){
+         return 2* Math.PI * this.radius
+    }
+
+    get diameter(){
+        return this.num * 2
+    }
+
+    get circumference(){
+        return 2 (Math.PI * this.num)
+    }
+
+    get area(){
+        return Math.PI (this.num * this.num)
+    }
+
+    set diameter(num){
+        return this.num * 2;
+    }
+
+
+    set circumference(num){
+        return 2 (Math.PI * this.num)
+    }
+
+    set area(num){
+        return Math.PI (this.num * thisnum)
+    }
+
+
+
+}
+
+
+const  circle = new Circle(6);
+
+
+circle.radius;
+
+circle.diameter;
+
+circle.circumference;
+
+circle.area;
+
+
+circle.diameter();
+circle.circumference();
+circle.area();
